89d3e0c2f72260b997289286a8fd6eac1c1c3f97
Anvar <66783845+crecker05ru@users.noreply.github.com>
2021-02-18 19:21:13 +0300

    calc

--- client-app/src/pages/ToolsPages.js
+++ client-app/src/pages/ToolsPages.js
@@ -1,34 +1,53 @@
 import {useState} from "react";
 import {NavigationBar} from "./NavigationBar";
 import {Footer} from "./Footer";
+import React from 'react'
 
-export const ToolsPages = () => {
+export const ToolsPages = (props) => {
 
-    const [firstName,setFirstName] = useState('')
-    const [lastName,setLastName] = useState('')
-    console.log(useState)
+    const [chainring, setChainring] = useState(38);
+    const [cassette, setCassette] = useState(11);
+    const [wheelsize, setWheelsize] = useState(2800);
+    const [cadence, setCadence] = useState(90);
 
-
-    function getFullName({ firstName, lastName }) {
-        //return `${firstName} ${lastName}`;
-        alert(`${firstName} ${lastName}`)
+    const calculateRatio = () => {
+        return  chainring / cassette
     }
 
-    return <div>
-        ToolsPage
-        <div>
-         <span>Input URL
-             <textarea name='inputUrl' value={firstName} onChange={e => setFirstName(e.target.value)}/>
-         </span>
-            <div></div>
-            <span>Output URL
-             <textarea name='outputUrl' value={lastName} />
-         </span>
-            <button name="submit" onClick={(event) => getFullName(firstName)}> Submit </button>
-        </div>
-        <NavigationBar />
-        <Footer />
-    </div>
+    const calculateSpeed = () => {
+    let result = 0.00006 * cadence * wheelsize * (chainring / cassette)
+        return result
+    }
 
+
+    return <div>
+            <div className="speedCalc">
+                <div className="speedCalc__container">
+                    <label>Chainring tooth </label>
+                    <input name="chainring"
+                           type="number"
+                           className="chainring"
+                           value={chainring}
+                           onChange={e => setChainring(e.target.value)}
+                    />
+                    <label>Cassette tooth </label>
+                    <input name="cassette" type="number"
+                           className="cassette" value={cassette}
+                           onChange={e => setCassette(e.target.value)}/>
+                    <label>Wheel size (in cm for circle) </label>
+                    <input name="wheelSize" type="number"
+                           className="wheelSize" value={wheelsize}
+                           onChange={e => setWheelsize(e.target.value)}/>
+                    <label>Cadence </label>
+                    <input name="cadence" type="number"
+                           className="cadence" value={cadence}
+                           onChange={e => setCadence(e.target.value)}/>
+                    <div className="gearsRatio">Gear ratio: {calculateRatio()}</div>
+                    <div className="speedResult">Km/h: {calculateSpeed()}</div>
+                </div>
+            </div>
+            <NavigationBar />
+            <Footer />
+    </div>
 
 }
